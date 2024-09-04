49e43031442a6c8e3f95afe5d7721e19625d9608
LucasPaladini <lucas.paladini@alumnos.ispi4038.edu.ar>
2024-09-04 20:40:41 -0300

    Clases Tarea y Ventana creadas

--- ListaTareasCodigo.py
+++ ListaTareasCodigo.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QApplication, QWidget, QLineEdit, QMessageBox, QPushButton, QTableView, QVBoxLayout
+from PySide6.QtWidgets import QApplication, QWidget, QMessageBox, QVBoxLayout
 from PySide6.QtGui import QStandardItemModel, QStandardItem
 from ListaTareas import Ui_Form
 
@@ -9,13 +9,9 @@ class Ventana(QWidget):
         self.ui = Ui_Form()
         self.ui.setupUi(self)
         self.setWindowTitle("Lista de Tareas")
-        self.layout = QVBoxLayout(self)
-        self.conectar_botones()
 
-        self.modelo_tareas = QStandardItemModel(self)
-        self.modelo_tareas_completadas = QStandardItemModel(self)
-        self.ui.tableTareaVacia.setModel(self.modelo_tareas)
-        self.ui.tableTareaCompletada.setModel(self.modelo_tareas_completadas)
+        self.layout = QVBoxLayout(self)
+        self.setLayout(self.layout)
 
         self.ui.tableTareaVacia.horizontalHeader().setVisible(False)
         self.ui.tableTareaVacia.verticalHeader().setVisible(False)
@@ -26,6 +22,17 @@ class Ventana(QWidget):
         self.header_completadas = self.ui.tableTareaCompletada.horizontalHeader()
         self.header_tareas.setStretchLastSection(True)
         self.header_completadas.setStretchLastSection(True)
+
+        self.modelo_tareas = QStandardItemModel(self)
+        self.modelo_tareas_completadas = QStandardItemModel(self)
+        self.ui.tableTareaVacia.setModel(self.modelo_tareas)
+        self.ui.tableTareaCompletada.setModel(self.modelo_tareas_completadas)
+
+
+class Tarea(Ventana):
+    def __init__(self):
+        super().__init__()
+        self.conectar_botones()
 
     def conectar_botones(self):
         self.ui.boton_agregar_tarea.clicked.connect(self.agregar_tarea)
@@ -41,34 +48,33 @@ class Ventana(QWidget):
         else:
             QMessageBox.warning(self, "Error", "El campo de texto está vacío")
 
-    def eliminar_tarea(self):
-        tareas = self.ui.tableTareaVacia.currentIndex()
-        tareas_completadas = self.ui.tableTareaCompletada.currentIndex()
-        if tareas.isValid():
-            self.modelo_tareas.removeRow(tareas.row())
-        elif tareas_completadas.isValid():
-            self.modelo_tareas_completadas.removeRow(tareas_completadas.row())
-        else:
-            QMessageBox.warning(self, "Error", "Primero selecciona la tarea.")
-
     def completar_tarea(self):
-        tarea_seleccionada = self.ui.tableTareaVacia.currentIndex()
-        tarea_completada_seleccionada = self.ui.tableTareaCompletada.currentIndex()
+        tarea_vacia = self.ui.tableTareaVacia.currentIndex()
+        tarea_completada = self.ui.tableTareaCompletada.currentIndex()
 
-        if tarea_seleccionada.isValid():
-            texto_tarea = self.modelo_tareas.itemFromIndex(tarea_seleccionada).text()
+        if tarea_vacia.isValid():
+            texto_tarea = self.modelo_tareas.itemFromIndex(tarea_vacia).text()
             self.modelo_tareas_completadas.appendRow(QStandardItem(texto_tarea))
-            self.modelo_tareas.removeRow(tarea_seleccionada.row())
-        elif tarea_completada_seleccionada.isValid():
+            self.modelo_tareas.removeRow(tarea_vacia.row())
+        elif tarea_completada.isValid():
             QMessageBox.warning(self, "Error", "La tarea ya está completada.")
         else:
             QMessageBox.warning(self, "Error", "Primero selecciona la tarea.")
 
-    def iniciar(self):
-        self.show()
+    def eliminar_tarea(self):
+        tarea_vacia = self.ui.tableTareaVacia.currentIndex()
+        tarea_completada = self.ui.tableTareaCompletada.currentIndex()
+
+        if tarea_vacia.isValid():
+            self.modelo_tareas.removeRow(tarea_vacia.row())
+        elif tarea_completada.isValid():
+            self.modelo_tareas_completadas.removeRow(tarea_completada.row())
+        else:
+            QMessageBox.warning(self, "Error", "Primero selecciona la tarea.")
 
 
-app = QApplication([])
-ventana = Ventana()
-ventana.iniciar()
-app.exec()
+if __name__ == "__main__":
+    app = QApplication([])
+    tarea = Tarea()
+    tarea.show()
+    app.exec()
