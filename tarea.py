0c788d7c699ea5786e2e33956d9871f7de7f8796
LucasPaladini <lucas.paladini@alumnos.ispi4038.edu.ar>
2024-09-20 23:24:40 -0300

    Modelo funcionando

--- tarea.py
+++ tarea.py
@@ -1,2 +1,11 @@
+
 class Tarea:
-    pass
+    def __init__(self, descripcion):
+        self.__descripcion = descripcion
+        print(self.__descripcion)
+
+    # def marcar_completada(self):
+    #     self.completada = True
+
+    def __str__(self):
+        return self.__descripcion
