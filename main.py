3f1ac9a7ed51ec5a1268ff9225eaca09cbbb5d3c
LucasPaladini <lucas.paladini@alumnos.ispi4038.edu.ar>
2024-09-20 23:16:24 -0300

    Eliminado librerias no utilizadas

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from PySide6.QtWidgets import QApplication
-from ventana import Ventana
 import sys
+from ventana import Ventana
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     ventana = Ventana()
     ventana.show()
     sys.exit(app.exec())
-    
+
+
+
+
